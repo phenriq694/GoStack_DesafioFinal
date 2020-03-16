575516bbb4aa14f2f1d99c0ff5cb0d9e8a84d7cc
Paulo Henrique <phenriq694@gmail.com>
2020-03-16 20:01:43 -0300

    BACKEND - Migration create files table, e configuração da model 'File' e controller 'FileController'.

--- backend/src/app/controllers/FileController.js
+++ backend/src/app/controllers/FileController.js
@@ -1,6 +1,16 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-    return res.json(req.file);
+    // req.file = retorna todas as informações do arquivo do upload.
+    const { originalname: name, filename: path } = req.file;
+
+    const file = await File.create({
+      name,
+      path,
+    });
+
+    return res.json(file);
   }
 }
 
