f5d908fd2726c64457039243fcbe209541703f1f
Paulo Henrique <phenriq694@gmail.com>
2020-03-16 19:57:31 -0300

    BACKEND - Configuração do multer, criação da rota e controller para manipulação de upload de arquivos.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -17,5 +22,7 @@ routes.put('/users', UserController.update);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:recipient_id', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
